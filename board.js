6cde5b9c05b0dae211e5f6e636c4941038775e44
hongsh26 <swatch6264@gmail.com>
2023-11-23 17:53:59 +0900

    231123 add js function

--- board.js
+++ board.js
@@ -11,21 +11,10 @@ class Board {
 function moveBoard() {
   location.href = "index.html";
 }
-//셀렉트 변경 알림
+//셀렉트 변경 알림(test)
 function changeSelect() {
   var value_category = document.getElementById("bd_category");
   alert(value_category.options[value_category.selectedIndex].value);
 }
 //페이지 작동 시켰을 때 작동하는 함수
-window.onload = function () {
-  document.getElementById("bd_title").innerHTML = "hello";
-  document.getElementById("bd_userId").innerHTML = "hong";
-  const pwElement = document.getElementById("pwd");
-  const loginButton = document.getElementById("loginBtn");
-  // loginButton.addEventListener("click", () => {
-  //   console.log(pwElement.value);
-  //   localStorage.setItem("pwd", "hello");
-  //   const abcd = localStorage.getItem("pwd");
-  //   console.log(abcd);
-  // });
-};
+window.onload = function () {};
