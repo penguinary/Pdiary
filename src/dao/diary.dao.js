e1881ec577fb931b37399e651037f42d7a5d3a89
YunDoll <20201106@vision.hoseo.edu>
2023-11-24 17:24:36 +0900

    fix diary.dao

--- src/dao/diary.dao.js
+++ src/dao/diary.dao.js
@@ -3,7 +3,7 @@ const logger = require('../config/logger')
 
 //get_main_dao
 
-async function postDiary(postDiary_req) {
+async function postDiary(req) {
     console.log(req.user_id);
     return new Promise((resolve, reject) => {
         var queryData = `insert into diary (user_id, diary_title, diary_category, diary_theme, diary_private, diary_date, diary_weather, diary_content) 
