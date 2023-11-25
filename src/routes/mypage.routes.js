f9a1a9134e926f59449576e17d844757a822173c
dlaejrgus908@gmail.com <20201091@vision.hoseo.edu>
2023-11-25 17:37:09 +0900

    231125 edit

--- src/routes/mypage.routes.js
+++ src/routes/mypage.routes.js
@@ -2,7 +2,9 @@ var express = require("express");
 const router = express.Router();
 const mypageController = require("../controller/mypage.controller");
 
-router.get("/", mypageController.getMyDiary);
+router.get("/", mypageController.getMyDiary, function(req, res){
+    res.sendFile(path.join(__dirname + "/src/public/mypage.html"))   
+});
 
 
 module.exports = router;
