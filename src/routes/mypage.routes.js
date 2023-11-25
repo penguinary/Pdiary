e83bd6a9fc3f08f2638a9983cf54c1213162ba6f
hongsh26 <swatch6264@gmail.com>
2023-11-26 02:04:30 +0900

    231126 good version

--- src/routes/mypage.routes.js
+++ src/routes/mypage.routes.js
@@ -2,20 +2,14 @@ var express = require("express");
 const router = express.Router();
 const mypageController = require("../controller/mypage.controller");
 
-// router.get("/", mypageController.getMyDiary, function(req, res){
-//     res.sendFile(path.join(__dirname + "/src/public/mypage.html"))   
-// });
+router.get("/", mypageController.getMyDiary, function(req, res){
+    res.sendFile(path.join(__dirname + "/src/public/mypage.html"))   
+});
 
 router.post("/", async function(req, res) {
     console.log("라우터 들어오나요")
     console.log(req.body);
     var result = await mypageController.updateData(req.body, res);
-    res.send(result);
-});
-
-router.get("/", async function(req, res) {
-    console.log("라우터", req.body);
-    var result = await mypageController.getData(req.body, res)
     res.send(result);
 });
 
