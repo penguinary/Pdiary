fd5d1d9ceac1c7be6970a3eff562905a5bb6593d
hongsh26 <swatch6264@gmail.com>
2023-11-27 02:46:54 +0900

    231127 last!!!

--- src/controller/diary.controller.js
+++ src/controller/diary.controller.js
@@ -2,8 +2,9 @@ const diaryService = require("../service/diary.service");
 
 //post_diary_controller
 async function postDiary(req, res, next) {
-    console.log("컨트롤러 들어옴");
-    const postDiary_req = req.body;
+    console.log("일기 쓰기 컨트롤러 들어옴");
+    const user_id = req.session.user_id;
+    const postDiary_req = { ...req.body, user_id };
     console.log("데이터 들어오는거 확인", postDiary_req);
     const postDiary_data = await diaryService.postDiary(postDiary_req);
     return res.status(postDiary_data.Status).json(postDiary_data);
